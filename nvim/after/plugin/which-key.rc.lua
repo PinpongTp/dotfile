2d27e74d9f22af26875183751927fc6ae1e31302
Pinpong Tongpat <pinpong.t@saimpiwat.com>
2022-07-05 22:10:19 +0700

    add more

--- nvim/after/plugin/which-key.rc.lua
+++ nvim/after/plugin/which-key.rc.lua
@@ -1,23 +1,14 @@
-if false then
-local status, wk = pcall(require, "which-key")
-if (not status) then return end
+if true then
+  local status, wk = pcall(require, "which-key")
+  if (not status) then return end
 
-wk.setup {
-  spelling = {
-    enable = false,
-    suggestions = 20
-  }
-}
+  wk.setup {spelling = {enable = false, suggestions = 20}}
 
-wk.register({
-  f = {
-    name = "file", -- optional group name
-    f = { "<cmd>Telescope find_files<cr>", "Find File" }, -- create a binding with label
-    r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File", noremap=false, buffer = 123 }, -- additional options for creating the keymap
-    n = { "New File" }, -- just a label. don't create any mapping
-    e = "Edit File", -- same as above
-    ["1"] = "which_key_ignore",  -- special label to hide it in the popup
-    b = { function() print("bar") end, "Foobar" } -- you can also pass functions!
-  },
-}, { prefix = "<leader>" })
+  wk.register({
+    ["<Leader>"] = {
+      c = {name = "+NERDCommant"},
+      t = {name = "+TAB"},
+      b = {name = "+BUFFER"}
+    }
+  })
 end
