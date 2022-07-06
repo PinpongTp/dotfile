43e2588574cb59f5918a279d490c4f8c500d8792
Pinpong Tongpat <pinpong.t@saimpiwat.com>
2022-07-06 21:20:19 +0700

    edit lualine and add tapline

--- nvim/after/plugin/bufferline.rc.lua
+++ nvim/after/plugin/bufferline.rc.lua
@@ -1,12 +1,15 @@
-local status, bufferline = pcall(require, "bufferline")
-if (not status) then return end
+if false then
+  local status, bufferline = pcall(require, "bufferline")
+  if (not status) then return end
 
-bufferline.setup {
-  options = {
-    -- mode = "tabs",
-    number = "ordinal",
-    indicator_icon = '▎',
-    left_trunc_marker = "",
-    right_trunc_marker = ""
+  bufferline.setup {
+    options = {
+      -- mode = "tabs",
+      number = "ordinal",
+      -- indicator_icon = '',
+      indicator_icon = '▎',
+      left_trunc_marker = "",
+      right_trunc_marker = ""
+    }
   }
-}
+end
