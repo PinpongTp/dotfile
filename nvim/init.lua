b5924ed594d012c78897efcf6b7c7782ab43d0fb
Pinpong Tongpat <pinpong.tp@gmail.com>
2022-07-10 21:17:35 +0700

    config all new to lua

---
+++ nvim/init.lua
@@ -0,0 +1,6 @@
+-- load require packer --
+require('vars') -- Variables
+require('opts') -- Options
+require('keys') -- Keymaps
+require('plug') -- Plugins
+require('plugin_configs') -- Plugins configs autoload
