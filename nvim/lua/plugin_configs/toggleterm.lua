63c535a329cf0297a5f60ce2b976d58d82995c9f
Pinpong Tongpat <pinpong.tp@gmail.com>
2022-07-11 20:11:06 +0700

    add config file_browser

--- nvim/lua/plugin_configs/toggleterm.lua
+++ nvim/lua/plugin_configs/toggleterm.lua
@@ -10,21 +10,6 @@ tg.setup {
   float_opts = {
     border = "curved",
     whiblend = 0,
-    highlights = {border = "Normal", backgroud = "Normal"}
-  },
-  extensions = {
-    file_browser = {
-      theme = "ivy",
-      -- disables netrw and use telescope-file-browser in its place
-      hijack_netrw = true,
-      mappings = {
-        ["i"] = {
-          -- your custom insert mode mappings
-        },
-        ["n"] = {
-          -- your custom normal mode mappings
-        },
-      },
-    },
+    highlights = { border = "Normal", backgroud = "Normal" }
   },
 }
