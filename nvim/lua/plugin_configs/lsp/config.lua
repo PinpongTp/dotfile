e350c45523c49e856deac676751cb60947d8ed5e
Pinpong Tongpat <pinpong.tp@gmail.com>
2022-07-11 15:48:01 +0700

    add sumneko_lua server lsp

--- nvim/lua/plugin_configs/lsp/config.lua
+++ nvim/lua/plugin_configs/lsp/config.lua
@@ -1,7 +1,7 @@
 local status, nvim_lsp = pcall(require, "lspconfig")
 if not status then return end
 local lsp_install = require('nvim-lsp-installer')
-local servers = {"tsserver", "vimls"}
+local servers = { "tsserver", "vimls", "sumneko_lua" }
 
 lsp_install.setup({
   ensure_installed = servers,
@@ -9,13 +9,13 @@ lsp_install.setup({
 
 
 for _, server in pairs(servers) do
-	local opts = {
-		on_attach = require("plugin_configs.lsp.handlers").on_attach,
-		capabilities = require("plugin_configs.lsp.handlers").capabilities,
-	}
-	local has_custom_opts, server_custom_opts = pcall(require, "plugin_configs.lsp.settings." .. server)
-	if has_custom_opts then
-		opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
-	end
-	nvim_lsp[server].setup(opts)
+  local opts = {
+    on_attach = require("plugin_configs.lsp.handlers").on_attach,
+    capabilities = require("plugin_configs.lsp.handlers").capabilities,
+  }
+  local has_custom_opts, server_custom_opts = pcall(require, "plugin_configs.lsp.settings." .. server)
+  if has_custom_opts then
+    opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
+  end
+  nvim_lsp[server].setup(opts)
 end
