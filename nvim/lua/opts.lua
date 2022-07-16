991108f4972f45ff3dbc26af2abf98f908407047
PinpongTp <pinpong.tp@gmail.com>
2022-07-16 17:36:49 +0700

    add opts.lua

--- nvim/lua/opts.lua
+++ nvim/lua/opts.lua
@@ -2,29 +2,39 @@
 local set = vim.opt
 local cmd = vim.api.nvim_command
 
--- [[ setup ]]
-set.number = true
-set.relativenumber = true
-set.title = true
+-- [[ Encoding ]]
+set.encoding = 'utf-8'
+set.fileencoding = 'utf-8'
+set.fileencodings = 'utf-8'
+
+-- [[ Tabs ]]
 set.autoindent = true
-set.ignorecase = true
 set.expandtab = true
 set.smarttab = true
 set.tabstop = 2
 set.shiftwidth = 2
+
+-- [[ Searching ]]
+set.hlsearch = true
+set.incsearch = true
+set.ignorecase = true
+
+-- [[ Clipboard sync ]]
+cmd('set clipboard+=unnamedplus') -- TODO set by lua
+
+-- [[ Setup ]]
 set.mouse = 'a'
+
 set.wrap = true
+--set.scrolloff = 999
+set.number = true
+set.relativenumber = true
+set.title = true
 set.colorcolumn = '120'
 --set.cmdheight = 2
 --set.cursorline = true
 cmd('au BufEnter * setlocal cursorline')
 cmd('au BufLeave * setlocal nocursorline')
-set.hlsearch = true
-set.incsearch = true
-set.scrolloff = 0
-set.fileencoding = 'utf-8'
-cmd('set clipboard+=unnamedplus') -- TODO set by lua
-
 
 -- [[ Theme ]]
 set.syntax = "ON"
