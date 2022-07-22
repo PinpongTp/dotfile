dbb98fd58387150408d0a1b326d0e18d6d3f8ecf
PinpongTp <pinpong.tp@gmail.com>
2022-07-22 18:07:48 +0700

    fig bug which-key

--- nvim/lua/plugin_configs/which-key.lua
+++ nvim/lua/plugin_configs/which-key.lua
@@ -1,14 +1,22 @@
 if true then
   local status, wk = pcall(require, "which-key")
   if (not status) then return end
+  local show = wk.show
+  wk.show = function(keys, opts)
+    if vim.bo.filetype == "TelescopePrompt" then
+      print(keys)
+      print(opts)
+    end
+    show(keys, opts)
+  end
 
-  wk.setup {spelling = {enable = false, suggestions = 20}}
+  wk.setup { spelling = { enable = false, suggestions = 20 }, default_mode = 'n' }
 
   wk.register({
     ["<Leader>"] = {
-      c = {name = "+NERDCommant"},
-      t = {name = "+TAB"},
-      b = {name = "+BUFFER"}
-    }
+      c = { name = "+NERDCommant" },
+      t = { name = "+TAB" },
+      b = { name = "+BUFFER" }
+    },
   })
 end
