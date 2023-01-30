a5e6f19cf2a4a51bc238baf645dc117c99f0f288
PinpongTp <pinpong.tp@gmail.com>
2023-01-30 22:58:25 +0700

    feat: load lazy in init file

--- nvim/lua/plug.lua
+++ nvim/lua/plug.lua
@@ -1,18 +1,5 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 -- start up --
-require('lazy').setup({
+return {
   -- for tmux navigator
   'christoomey/vim-tmux-navigator',
 
@@ -134,4 +121,4 @@ require('lazy').setup({
   'tyru/open-browser.vim',
   'weirongxu/plantuml-previewer.vim'
 
-})
+}
