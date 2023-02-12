182bca92faae3f416a94bdc0d1a69807329c9bf4
PinpongTp <pinpong.tp@gmail.com>
2023-02-12 22:36:33 +0700

    feat: add cmp

--- nvim/lua/plugin_configs/lsp/init.lua
+++ nvim/lua/plugin_configs/lsp/init.lua
@@ -1,38 +1,35 @@
 local ok, _ = pcall(require, "lsp-zero")
 if not ok then
-	return
+  return
 end
 
 --print('test load lsp-zero')
 
 local lsp = require("lsp-zero")
-lsp.preset("recommended")
-
-lsp.set_preferences({
-	set_lsp_keymaps = false,
+lsp.preset({
+  name = 'minimal',
+  set_lsp_keymaps = false,
 })
-
 lsp.nvim_workspace()
-
 lsp.setup()
 
 local signs = {
-	{ name = "DiagnosticSignError", text = "" },
-	{ name = "DiagnosticSignWarn", text = "" },
-	{ name = "DiagnosticSignHint", text = "" },
-	{ name = "DiagnosticSignInfo", text = "" },
+  { name = "DiagnosticSignError", text = "" },
+  { name = "DiagnosticSignWarn", text = "" },
+  { name = "DiagnosticSignHint", text = "" },
+  { name = "DiagnosticSignInfo", text = "" },
 }
 
 vim.diagnostic.config({
-	virtual_text = true,
-	signs = true,
-	update_in_insert = false,
-	underline = true,
-	severity_sort = false,
-	float = true,
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
 })
 
---require 'plugin_configs.lsp.cmp'
+require 'plugin_configs.lsp.cmp'
 require("plugin_configs.lsp.config")
 require("plugin_configs.lsp.null-ls")
 require("plugin_configs.lsp.saga")
