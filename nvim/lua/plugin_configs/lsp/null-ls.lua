497b393e841f4b692993097e8c7e232b2e11bb27
PinpongTp <pinpong.tp@gmail.com>
2023-02-12 17:15:24 +0700

    feat: final migrate to lazyvim

--- nvim/lua/plugin_configs/lsp/null-ls.lua
+++ nvim/lua/plugin_configs/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local ok, null_ls = pcall(require, "null-ls")
 if not ok then
-  return
+	return
 end
 
 --null_ls.setup {
@@ -13,13 +13,13 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-  on_attach = require("plugin_configs.lsp.handlers").on_attach,
-  debug = true,
-  sources = {
-    formatting.prettier.with({ extra_args = { "--single-quote" } }),
-    formatting.stylua,
-    diagnostics.eslint,
-    null_ls.builtins.code_actions.eslint,
-    null_ls.builtins.code_actions.gitsigns
-  },
+	on_attach = require("plugin_configs.lsp.handlers").on_attach,
+	debug = true,
+	sources = {
+		formatting.prettier.with({ extra_args = { "--single-quote" } }),
+		formatting.stylua,
+		diagnostics.eslint,
+		null_ls.builtins.code_actions.eslint,
+		null_ls.builtins.code_actions.gitsigns,
+	},
 })
