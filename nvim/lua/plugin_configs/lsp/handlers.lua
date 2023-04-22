a59472dafd81af24b2f04ab21b1736fb8126b0f2
PinpongTp <pinpong.tp@gmail.com>
2023-04-23 00:33:50 +0700

    feat: update lsp.buf.format for nvim 0.9

--- nvim/lua/plugin_configs/lsp/handlers.lua
+++ nvim/lua/plugin_configs/lsp/handlers.lua
@@ -49,8 +49,9 @@ local function auto_format(client)
 	if client.server_capabilities.documentFormattingProvider then
 		vim.api.nvim_command([[augroup Format]])
 		vim.api.nvim_command([[autocmd! * <buffer>]])
-		vim.api.nvim_command([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]])
-		--vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+		--vim.api.nvim_command([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]])
+    --vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
 		-- vim. Isp. buf. formatting seg sync is deprecated. Use vim. Isp.buf. format instead
 		--vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
 		vim.api.nvim_command([[augroup END]])
