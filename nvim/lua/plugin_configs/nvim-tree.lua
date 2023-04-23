2c6c133dd8ddf3b1805458af8ae992c198aeb972
PinpongTp <pinpong.tp@gmail.com>
2023-04-23 15:19:31 +0700

    feat: config nvim-tree width

--- nvim/lua/plugin_configs/nvim-tree.lua
+++ nvim/lua/plugin_configs/nvim-tree.lua
@@ -1,79 +1,69 @@
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  remove_keymaps = {
-    "s"
-  },
-  view = {
-    adaptive_size = false,
-    width = 30,
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = "<Tab>", action = ":bnext<CR>" },
-        { key = "u", action = "dir_up" },
-        { key = "?", action = "toggle_help" },
-      },
-    },
-  },
-  renderer = {
-    group_empty = true,
-    highlight_git = false,
-    indent_markers = {
-      enable = true,
-      icons = {
-        corner = "└",
-        edge = "│",
-        item = "│",
-        none = " ",
-      },
-    },
-    icons = {
-      show = {
-        folder_arrow = false
-      },
-      glyphs = {
-        default = "",
-        symlink = "",
-        bookmark = "",
-        folder = {
-          arrow_closed = "",
-          arrow_open = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
-        },
-        git = {
-          unstaged = "✗",
-          staged = "✓",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "★",
-          deleted = "",
-          ignored = "◌",
-        },
-      },
-    },
-  },
-  update_focused_file = {
-    enable = true,
-    update_root = true,
-    ignore_list = {},
-  },
-  diagnostics = {
-    enable = false,
-    show_on_dirs = false,
-    debounce_delay = 50,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-  filters = {
-    dotfiles = false,
-  },
+	on_attach = on_attach,
+	sort_by = "case_sensitive",
+	view = {
+		adaptive_size = false,
+		width = 40,
+	},
+	renderer = {
+		group_empty = true,
+		highlight_git = false,
+		indent_markers = {
+			enable = true,
+			icons = {
+				corner = "└",
+				edge = "│",
+				item = "│",
+				none = " ",
+			},
+		},
+		icons = {
+			show = {
+				folder_arrow = false,
+			},
+			glyphs = {
+				default = "",
+				symlink = "",
+				bookmark = "",
+				folder = {
+					arrow_closed = "",
+					arrow_open = "",
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+					symlink_open = "",
+				},
+				git = {
+					unstaged = "✗",
+					staged = "✓",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "★",
+					deleted = "",
+					ignored = "◌",
+				},
+			},
+		},
+	},
+	update_focused_file = {
+		enable = true,
+		update_root = true,
+		ignore_list = {},
+	},
+	diagnostics = {
+		enable = false,
+		show_on_dirs = false,
+		debounce_delay = 50,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	filters = {
+		dotfiles = false,
+	},
 })
