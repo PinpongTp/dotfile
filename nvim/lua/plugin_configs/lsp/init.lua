f843a3e95fc77edff3c133c063febc9efe45149b
PinpongTp <pinpong.tp@gmail.com>
2023-04-23 15:19:15 +0700

    feat: add fidget package

--- nvim/lua/plugin_configs/lsp/init.lua
+++ nvim/lua/plugin_configs/lsp/init.lua
@@ -1,32 +1,32 @@
 local ok, _ = pcall(require, "lsp-zero")
 if not ok then
-  return
+	return
 end
 
 --print('test load lsp-zero')
 
 local lsp = require("lsp-zero")
 lsp.preset({
-  name = 'minimal',
-  set_lsp_keymaps = false,
+	name = "minimal",
+	set_lsp_keymaps = false,
 })
 lsp.nvim_workspace()
 lsp.setup()
 
 local signs = {
-  { name = "DiagnosticSignError", text = "" },
-  { name = "DiagnosticSignWarn", text = "" },
-  { name = "DiagnosticSignHint", text = "" },
-  { name = "DiagnosticSignInfo", text = "" },
+	{ name = "DiagnosticSignError", text = "" },
+	{ name = "DiagnosticSignWarn", text = "" },
+	{ name = "DiagnosticSignHint", text = "" },
+	{ name = "DiagnosticSignInfo", text = "" },
 }
 
 vim.diagnostic.config({
-  virtual_text = true,
-  signs = true,
-  update_in_insert = false,
-  underline = true,
-  severity_sort = false,
-  float = true,
+	virtual_text = true,
+	signs = true,
+	update_in_insert = false,
+	underline = true,
+	severity_sort = false,
+	float = true,
 })
 
 require("plugin_configs.lsp.cmp")
@@ -34,3 +34,4 @@ require("plugin_configs.lsp.config")
 require("plugin_configs.lsp.null-ls")
 require("plugin_configs.lsp.saga")
 require("plugin_configs.lsp.toggle-lsp-diagnostics")
+require("plugin_configs.lsp.fidget")
