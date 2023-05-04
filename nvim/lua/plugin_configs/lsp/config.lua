0ec85add5cf2907beb906e9950b1c932343ec4d0
PinpongTp <pinpong.tp@gmail.com>
2023-05-04 16:10:12 +0700

    feat: update lsp config

--- nvim/lua/plugin_configs/lsp/config.lua
+++ nvim/lua/plugin_configs/lsp/config.lua
@@ -1,21 +1,21 @@
-local status, nvim_lsp = pcall(require, "lspconfig")
-if not status then return end
---local lsp_install = require('nvim-lsp-installer')
 local servers = { "tsserver", "vimls", "lua_ls", "gopls", "vuels", "html" }
 
---lsp_install.setup({
---ensure_installed = servers,
---})
+local status, nvim_lsp = pcall(require, "lspconfig")
+if not status then
+  return
+end
 
-
-for _, server in pairs(servers) do
+local handlers = require("plugin_configs.lsp.handlers")
+for _, server in ipairs(servers) do
   local opts = {
-    on_attach = require("plugin_configs.lsp.handlers").on_attach,
-    capabilities = require("plugin_configs.lsp.handlers").capabilities,
+      on_attach = handlers.on_attach,
+      capabilities = handlers.capabilities,
   }
+
   local has_custom_opts, server_custom_opts = pcall(require, "plugin_configs.lsp.settings." .. server)
   if has_custom_opts then
     opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
   end
+
   nvim_lsp[server].setup(opts)
 end
