9503942573a2c50e935a871dc1a4bd6e78a9fdba
PinpongTp <pinpong.tp@gmail.com>
2023-05-04 14:59:45 +0700

    feat: formating

--- nvim/lua/plugin_configs/lsp/saga.lua
+++ nvim/lua/plugin_configs/lsp/saga.lua
@@ -1,23 +1,20 @@
-local saga = require 'lspsaga'
+local saga = require("lspsaga")
 
-saga.init_lsp_saga {
-  error_sign = '',
-  warn_sign = '',
-  hint_sign = '',
-  infor_sign = '',
-  border_style = "round",
-  finder_action_keys = {
-    open = "o",
-    vsplit = "s",
-    split = "v",
-    tabe = "t",
-    quit = "q",
-  },
-  server_filetype_map = {
+saga.init_lsp_saga({
+    error_sign = "",
+    warn_sign = "",
+    hint_sign = "",
+    infor_sign = "",
+    border_style = "round",
+    finder_action_keys = {
+        open = "o",
+        vsplit = "s",
+        split = "v",
+        tabe = "t",
+        quit = "q",
+    },
+    server_filetype_map = {},
+    definition_preview_icon = "  ",
+})
 
-  },
-  definition_preview_icon = "  ",
-}
-
-
-local action = require 'lspsaga.codeaction'
+local action = require("lspsaga.codeaction")
