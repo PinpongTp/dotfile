ee2790053c738b6466305ec32f2b7490c3728955
PinpongTp <pinpong.tp@gmail.com>
2023-05-04 16:46:44 +0700

    feat: update mason config

--- nvim/lua/plugin_configs/lsp/config.lua
+++ nvim/lua/plugin_configs/lsp/config.lua
@@ -1,21 +1,38 @@
 local servers = { "tsserver", "vimls", "lua_ls", "gopls", "vuels", "html" }
 
+local mason = require("mason")
+local mason_lsp = require("mason")
+
+mason.setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
+})
+
+mason_lsp.setup({
+	ensure_installed = servers,
+})
+
 local status, nvim_lsp = pcall(require, "lspconfig")
 if not status then
-  return
+	return
 end
 
 local handlers = require("plugin_configs.lsp.handlers")
 for _, server in ipairs(servers) do
-  local opts = {
-      on_attach = handlers.on_attach,
-      capabilities = handlers.capabilities,
-  }
+	local opts = {
+		on_attach = handlers.on_attach,
+		capabilities = handlers.capabilities,
+	}
 
-  local has_custom_opts, server_custom_opts = pcall(require, "plugin_configs.lsp.settings." .. server)
-  if has_custom_opts then
-    opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
-  end
+	local has_custom_opts, server_custom_opts = pcall(require, "plugin_configs.lsp.settings." .. server)
+	if has_custom_opts then
+		opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
+	end
 
-  nvim_lsp[server].setup(opts)
+	nvim_lsp[server].setup(opts)
 end
