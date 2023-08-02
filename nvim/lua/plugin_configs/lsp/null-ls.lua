b5c81b27c52428fb63fe95da2206d96dda5b1aa3
PinpongTp <pinpong.tp@gmail.com>
2023-08-03 01:21:51 +0700

    feat: update dionoc for code actions

--- nvim/lua/plugin_configs/lsp/null-ls.lua
+++ nvim/lua/plugin_configs/lsp/null-ls.lua
@@ -9,12 +9,15 @@ local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
 	on_attach = require("plugin_configs.lsp.handlers").on_attach,
-	debug = true,
+	--debug = true,
 	sources = {
 		formatting.prettierd.with({ extra_args = { "--single-quote" } }),
 		formatting.stylua,
 		diagnostics.eslint_d,
-		code_actions.eslint_d,
+		code_actions.eslint_d.with({
+			method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+			-- todo code_actions on save only
+		}),
 		--code_actions.gitsigns,
 	},
 })
