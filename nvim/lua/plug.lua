4f0364bbe96d5c5b913af86d80f61e410affb722
PinpongTp <pinpong.tp@gmail.com>
2023-08-03 01:19:59 +0700

    feat: add dock dock animation

--- nvim/lua/plug.lua
+++ nvim/lua/plug.lua
@@ -34,6 +34,18 @@ return {
 		end,
 	},
 	{
+		"tamton-aquib/duck.nvim",
+		config = function()
+			vim.keymap.set("n", "<leader>dd", function()
+				require("duck").hatch()
+			end, {})
+			vim.keymap.set("n", "<leader>dk", function()
+				require("duck").cook()
+			end, {})
+		end,
+	},
+
+	{
 		"pgdouyon/vim-yin-yang",
 		priority = 1001,
 		config = function()
