fa2d9e8b4dc9f86d4f11ef934f3379253a177103
PinpongTp <pinpong.tp@gmail.com>
2023-08-04 02:18:52 +0700

    feat: config fold

--- nvim/lua/plug.lua
+++ nvim/lua/plug.lua
@@ -292,6 +292,35 @@ return {
 
 	-- helper
 	"akinsho/toggleterm.nvim",
+
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = {
+			"kevinhwang91/promise-async",
+			{
+				"luukvbaal/statuscol.nvim",
+				config = function()
+					local builtin = require("statuscol.builtin")
+					require("statuscol").setup({
+						relculright = true,
+						segments = {
+							{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+							{ text = { "%s" }, click = "v:lua.ScSa" },
+							{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+						},
+					})
+				end,
+			},
+		},
+		config = function()
+			require("ufo").setup({
+				provider_selector = function(bufnr, filetype, buftype)
+					return { "treesitter", "indent" }
+				end,
+			})
+		end,
+	},
+
 	"voldikss/vim-floaterm",
 	{ "folke/which-key.nvim", lazy = false },
 	"kshenoy/vim-signature",
