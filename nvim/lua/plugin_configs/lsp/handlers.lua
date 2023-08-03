53cae94feaac5b69769c0506ec64bdf82b1ebb01
PinpongTp <pinpong.tp@gmail.com>
2023-08-04 02:18:01 +0700

    feat: comment notify

--- nvim/lua/plugin_configs/lsp/handlers.lua
+++ nvim/lua/plugin_configs/lsp/handlers.lua
@@ -57,7 +57,7 @@ navic.setup({ depth_limit = 3, highlight = true })
 local navbuddy = require("nvim-navbuddy")
 
 M.on_attach = function(client, bufnr)
-	vim.notify(client.name .. " starting...")
+	--vim.notify(client.name .. " starting...")
 	local cap = client.server_capabilities
 
 	if cap.documentSymbolProvider then
