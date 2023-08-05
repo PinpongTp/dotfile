f20b85b7033597de2a49bf586e8065d1c0625fd1
PinpongTp <pinpong.tp@gmail.com>
2023-08-06 02:01:27 +0700

    feat: update icon and add comment

--- nvim/lua/plugin_configs/lsp/saga.lua
+++ nvim/lua/plugin_configs/lsp/saga.lua
@@ -11,7 +11,7 @@ saga.setup({
 		winblend = 0,
 		expand = "",
 		collapse = "",
-		code_action = "💡",
+		code_action = "",
 		incoming = " ",
 		outgoing = " ",
 		hover = "",
@@ -30,29 +30,29 @@ saga.setup({
 
 local keymap = vim.keymap.set
 --keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>")
-keymap("n", "gh", "<cmd>Lspsaga finder<CR>")
+keymap("n", "gh", "<cmd>Lspsaga finder<CR>") --  get help
 keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
-keymap({ "n", "v" }, "ga", "<cmd>Lspsaga code_action<CR>")
+keymap({ "n", "v" }, "ga", "<cmd>Lspsaga code_action<CR>") -- get action
 --keymap("n", "gr", "<cmd>Lspsaga rename<CR>")
-keymap("n", "gr", "<cmd>Lspsaga rename ++project<CR>")
+keymap("n", "gr", "<cmd>Lspsaga rename ++project<CR>") -- get rename
 keymap("n", "gp", "<cmd>Lspsaga peek_definition<CR>")
-keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
+keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>") -- get goto definition
 --keymap("n", "gt", "<cmd>Lspsaga peek_type_definition<CR>")
 --keymap("n", "gt", "<cmd>Lspsaga goto_type_definition<CR>")
 keymap("n", "<Leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>")
 keymap("n", "<Leader>sb", "<cmd>Lspsaga show_buf_diagnostics<CR>")
 keymap("n", "<Leader>sw", "<cmd>Lspsaga show_workspace_diagnostics<CR>")
 keymap("n", "<Leader>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
-keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
-keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
-keymap("n", "[E", function()
+keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>") -- prev error or warning
+keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>") -- next error or warning
+keymap("n", "[E", function() -- prev error
 	require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
 end)
-keymap("n", "]E", function()
+keymap("n", "]E", function() -- next error
 	require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
 end)
-keymap("n", "<leader>O", "<cmd>Lspsaga outline<CR>")
-keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>")
+keymap("n", "<leader>O", "<cmd>Lspsaga outline<CR>") -- get outline
+keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>") -- hover doc
 keymap("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
 keymap("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
 keymap({ "n", "t" }, "<A-d>", "<cmd>Lspsaga term_toggle<CR>")
