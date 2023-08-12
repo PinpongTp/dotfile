6cc07d37a68ea28fbaabf474cc473244ca57e6ad
PinpongTp <pinpong.tp@gmail.com>
2023-08-12 14:39:19 +0700

    feat: set default indent to false

--- nvim/lua/plugin_configs/indent-blankline.lua
+++ nvim/lua/plugin_configs/indent-blankline.lua
@@ -1,5 +1,7 @@
 --vim.cmd([[highlight IndentBlanklineIndent1 guifg=#333333 gui=nocombine]])
 
+vim.cmd([[let g:indent_blankline_enabled = v:false]])
+
 require("indent_blankline").setup({
 	show_current_context = true,
 	show_current_context_start = true,
