4353d15a59c52a14630c8e3a65149c026c6f800e
PinpongTp <pinpong.tp@gmail.com>
2023-08-19 21:05:17 +0700

    feat: update some which-key

--- nvim/lua/plugin_configs/which-key.lua
+++ nvim/lua/plugin_configs/which-key.lua
@@ -1,22 +1,37 @@
 if true then
-  local status, wk = pcall(require, "which-key")
-  if (not status) then return end
-  local show = wk.show
-  wk.show = function(keys, opts)
-    if vim.bo.filetype == "TelescopePrompt" then
-      print(keys)
-      print(opts)
-    end
-    show(keys, opts)
-  end
+	local status, wk = pcall(require, "which-key")
+	if not status then
+		return
+	end
+	local show = wk.show
+	wk.show = function(keys, opts)
+		if vim.bo.filetype == "TelescopePrompt" then
+			print(keys)
+			print(opts)
+		end
+		show(keys, opts)
+	end
 
-  wk.setup { spelling = { enable = false, suggestions = 20 }, default_mode = 'n' }
+	wk.setup({ spelling = { enable = false, suggestions = 20 }, default_mode = "n" })
 
-  wk.register({
-    ["<Leader>"] = {
-      c = { name = "+NERDCommant" },
-      t = { name = "+TAB" },
-      b = { name = "+BUFFER" }
-    },
-  })
+	wk.register({
+		["<Leader>"] = {
+			e = { name = "Exp" },
+			c = { name = "+Commant" },
+			t = { name = "+Tab" },
+			b = { name = "+Buffer" },
+			s = { name = "+Saga" },
+			f = { name = "+FindText" },
+		},
+		[";"] = {
+			f = { name = "Find & Telescope", f = { "find file" }, l = { "live grep" } },
+			t = {
+				name = "Toggle",
+				g = { "git signs" },
+				n = { "line numbers" },
+				i = { "indent lines" },
+				t = { "floaterm" },
+			},
+		},
+	})
 end
