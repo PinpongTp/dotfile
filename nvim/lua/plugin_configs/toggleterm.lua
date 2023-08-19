af140ca4e673c394838c3b0f66d401e2e96303c5
PinpongTp <pinpong.tp@gmail.com>
2023-08-19 21:01:35 +0700

    feat: use togglterm for lazyvim

--- nvim/lua/plugin_configs/toggleterm.lua
+++ nvim/lua/plugin_configs/toggleterm.lua
@@ -1,15 +1,26 @@
 local status, tg = pcall(require, "toggleterm")
-if (not status) then return end
+if not status then
+	return
+end
 
-tg.setup {
-  open_mapping = [[<c-\>]],
+tg.setup({
+	open_mapping = [[<c-t>]],
 
-  shade_terminals = true,
-  close_on_exit = true,
-  direction = "float",
-  float_opts = {
-    border = "curved",
-    whiblend = 0,
-    highlights = { border = "Normal", backgroud = "Normal" }
-  },
-}
+	shade_terminals = true,
+	close_on_exit = true,
+	direction = "float",
+	float_opts = {
+		border = "curved",
+		whiblend = 0,
+		highlights = { border = "Normal", backgroud = "Normal" },
+	},
+})
+
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _lazygit_toggle()
+	lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", ";tg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
