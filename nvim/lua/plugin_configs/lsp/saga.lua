d4f8c940c6e32d233888832e051f98045b0ae763
PinpongTp <pinpong.tp@gmail.com>
2023-08-29 00:35:26 +0700

    feat: update to use normal goto def from lsp buf

--- nvim/lua/plugin_configs/lsp/saga.lua
+++ nvim/lua/plugin_configs/lsp/saga.lua
@@ -35,10 +35,13 @@ keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
 keymap({ "n", "v" }, "ga", "<cmd>Lspsaga code_action<CR>") -- get action
 --keymap("n", "gr", "<cmd>Lspsaga rename<CR>")
 keymap("n", "gr", "<cmd>Lspsaga rename ++project<CR>") -- get rename
+keymap("n", "<space>rn", vim.lsp.buf.rename)
+keymap("n", "gl", vim.lsp.buf.references)
+vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help)
 keymap("n", "gp", "<cmd>Lspsaga peek_definition<CR>")
-keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>") -- get goto definition
+--keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>") -- get goto definition
+keymap("n", "gd", vim.lsp.buf.definition)
 --keymap("n", "gt", "<cmd>Lspsaga peek_type_definition<CR>")
---keymap("n", "gt", "<cmd>Lspsaga goto_type_definition<CR>")
 keymap("n", "<Leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>")
 keymap("n", "<Leader>sb", "<cmd>Lspsaga show_buf_diagnostics<CR>")
 keymap("n", "<Leader>sw", "<cmd>Lspsaga show_workspace_diagnostics<CR>")
