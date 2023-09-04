85a11d07160fd78f945889e6f4f5c2ef8f03d072
PinpongTp <pinpong.tp@gmail.com>
2023-09-04 22:56:56 +0700

    feat: add edgy

--- nvim/lua/plug.lua
+++ nvim/lua/plug.lua
@@ -468,6 +468,62 @@ return {
 			})
 		end,
 	},
+	{
+		"folke/edgy.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.opt.laststatus = 3
+			vim.opt.splitkeep = "screen"
+		end,
+		opts = {
+			left = {
+				{
+					ft = "NvimTree",
+					open = "NvimTreeOpen",
+				},
+				{
+					ft = "Outline",
+					open = "SymbolsOutlineOpen",
+				},
+			},
+			bottom = {},
+			right = {},
+			top = {},
+			options = {
+				left = { size = 35 },
+				bottom = {},
+				right = {},
+				top = {},
+			},
+			animate = {
+				enabled = false,
+			},
+			exit_when_last = true,
+			keys = {
+				["<C-Right>"] = function(win)
+					win:resize("width", 2)
+				end,
+				["<C-Left>"] = function(win)
+					win:resize("width", -2)
+				end,
+				["<C-Up>"] = function(win)
+					win:resize("height", 2)
+				end,
+				["<C-Down>"] = function(win)
+					win:resize("height", -2)
+				end,
+			},
+		},
+		keys = {
+			{
+				"<leader>p",
+				function()
+					require("edgy").toggle()
+				end,
+				desc = "Toggle Edgy",
+			},
+		},
+	},
 
 	-- for move
 	{
