f5c640bcd01aab852bb514aee1b6153f15100c43
PinpongTp <pinpong.tp@gmail.com>
2023-09-10 20:57:01 +0700

    feat: update icons and defailt mapping

--- nvim/lua/plugin_configs/navbuddy.lua
+++ nvim/lua/plugin_configs/navbuddy.lua
@@ -2,6 +2,12 @@ local navbuddy = require("nvim-navbuddy")
 local actions = require("nvim-navbuddy.actions")
 local icons = require("ui.icons")
 
+-- loop icons and add space after each
+local icons_space = {}
+for k, v in pairs(icons) do
+	icons_space[k] = v .. " "
+end
+
 navbuddy.setup({
 	window = {
 		border = "single", -- "rounded", "double", "solid", "none"
@@ -36,48 +42,50 @@ navbuddy.setup({
 			branch = " ",
 		},
 	},
-	icons = icons,
+	icons = icons_space,
 	use_default_mappings = true, -- If set to false, only mappings set
-	-- by user are set. Else default
-	-- mappings are used for keys
-	-- that are not set by user
 	mappings = {
-		["<esc>"] = actions.close, -- Close and cursor to original location
-		["q"] = actions.close,
+		["<esc>"] = actions.close(), -- Close and cursor to original location
+		["q"] = actions.close(),
 
-		["j"] = actions.next_sibling, -- down
-		["k"] = actions.previous_sibling, -- up
+		["j"] = actions.next_sibling(), -- down
+		["k"] = actions.previous_sibling(), -- up
 
-		["h"] = actions.parent, -- Move to left panel
-		["l"] = actions.children, -- Move to right panel
-		["0"] = actions.root, -- Move to first panel
+		["h"] = actions.parent(), -- Move to left panel
+		["l"] = actions.children(), -- Move to right panel
+		["0"] = actions.root(), -- Move to first panel
 
-		["v"] = actions.visual_name, -- Visual selection of name
-		["V"] = actions.visual_scope, -- Visual selection of scope
+		["v"] = actions.visual_name(), -- Visual selection of name
+		["V"] = actions.visual_scope(), -- Visual selection of scope
 
-		["y"] = actions.yank_name, -- Yank the name to system clipboard "+
-		["Y"] = actions.yank_scope, -- Yank the scope to system clipboard "+
+		["y"] = actions.yank_name(), -- Yank the name to system clipboard "+
+		["Y"] = actions.yank_scope(), -- Yank the scope to system clipboard "+
 
-		["i"] = actions.insert_name, -- Insert at start of name
-		["I"] = actions.insert_scope, -- Insert at start of scope
+		["i"] = actions.insert_name(), -- Insert at start of name
+		["I"] = actions.insert_scope(), -- Insert at start of scope
 
-		["a"] = actions.append_name, -- Insert at end of name
-		["A"] = actions.append_scope, -- Insert at end of scope
+		["a"] = actions.append_name(), -- Insert at end of name
+		["A"] = actions.append_scope(), -- Insert at end of scope
 
-		["r"] = actions.rename, -- Rename currently focused symbol
+		["r"] = actions.rename(), -- Rename currently focused symbol
 
-		["d"] = actions.delete, -- Delete scope
+		["d"] = actions.delete(), -- Delete scope
 
-		["f"] = actions.fold_create, -- Create fold of current scope
-		["F"] = actions.fold_delete, -- Delete fold of current scope
+		["f"] = actions.fold_create(), -- Create fold of current scope
+		["F"] = actions.fold_delete(), -- Delete fold of current scope
 
-		["c"] = actions.comment, -- Comment out current scope
+		["c"] = actions.comment(), -- Comment out current scope
 
-		["<enter>"] = actions.select, -- Goto selected symbol
-		["o"] = actions.select,
+		["<enter>"] = actions.select(), -- Goto selected symbol
+		["o"] = actions.select(),
 
-		["J"] = actions.move_down, -- Move focused node down
-		["K"] = actions.move_up, -- Move focused node up
+		["J"] = actions.move_down(), -- Move focused node down
+		["K"] = actions.move_up(), -- Move focused node up
+
+		["s"] = actions.toggle_preview(), -- Show preview of current node
+
+		["<C-v>"] = actions.vsplit(), -- Open selected node in a vertical split
+		["<C-s>"] = actions.hsplit(), -- Open selected node in a horizontal split
 
 		["t"] = actions.telescope({ -- Fuzzy finder at current level.
 			layout_config = { -- All options that can be
@@ -88,6 +96,8 @@ navbuddy.setup({
 			},
 			layout_strategy = "horizontal",
 		}),
+
+		["g?"] = actions.help(), -- Open mappings help window
 	},
 	lsp = {
 		auto_attach = false, -- If set to true, you don't need to manually use attach function
