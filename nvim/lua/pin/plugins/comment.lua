b2facc0e7e9f7dd430529d0ab4de97cd5bacff5a
PinpongTp <pinpong.tp@gmail.com>
2024-04-06 02:26:53 +0700

    feat: config comment

---
+++ nvim/lua/pin/plugins/comment.lua
@@ -0,0 +1,18 @@
+return {
+  "numToStr/Comment.nvim",
+  event = {"BufReadPre", "BufNewFile"},
+  dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+  config = function()
+    local comment = require("Comment")
+
+    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+
+    comment.setup({
+      keybindings = {
+        basic = true,
+        extra = true,
+      },
+      pre_hook = ts_context_commentstring.create_pre_hook(),
+    })
+  end,
+}
