09d9e3115cd7bc27f816f8b342ef37b4477ad8c7
PinpongTp <pinpong.tp@gmail.com>
2024-04-05 18:02:44 +0700

    feat: update tmux config

--- nvim/lua/plug.lua
+++ nvim/lua/plug.lua
@@ -1,7 +1,16 @@
 -- start up --
 return {
 	-- for tmux navigator
-	{ "christoomey/vim-tmux-navigator" },
+	{
+		"christoomey/vim-tmux-navigator",
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
 	{
 		"mrjones2014/smart-splits.nvim",
 		config = function()
