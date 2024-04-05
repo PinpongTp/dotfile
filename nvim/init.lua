797457f341d91f8a4010df414e631531bafc44f1
PinpongTp <pinpong.tp@gmail.com>
2024-04-06 00:35:06 +0700

    feat: create new setup

---
+++ nvim/init.lua
@@ -0,0 +1,2 @@
+require("pin.core")
+require("pin.lazy")
---
+++ nvim/lua/pin/core/options.lua
@@ -0,0 +1,37 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+-- tabs and indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+
+-- TODO change This
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+
+-- color
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
