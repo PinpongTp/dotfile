c1ae026e37e4092283f2d78db70a44eb45cefdd7
PinpongTp <pinpong.tp@gmail.com>
2024-04-06 20:44:57 +0700

    feat: add color

--- nvim/lua/pin/plugins/init.lua
+++ nvim/lua/pin/plugins/init.lua
@@ -3,4 +3,12 @@ return {
 	"christoomey/vim-tmux-navigator",
 	-- "tyru/open-browser.vim",
 	-- "weirongxu/plantuml-previewer.vim",
+	{
+		--"norcalli/nvim-colorizer.lua",
+		"NvChad/nvim-colorizer.lua",
+		lazy = false,
+		config = function()
+			require("colorizer").setup({})
+		end,
+	},
 }
