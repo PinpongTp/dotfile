da47dd8b71829fac164e25383e770b048955efe3
PinpongTp <pinpong.tp@gmail.com>
2024-04-06 19:24:54 +0700

    feat: clean code

--- nvim/lua/pin/core/keymaps.lua
+++ nvim/lua/pin/core/keymaps.lua
@@ -8,6 +8,8 @@ keymap.set("n", ";tn", ":set rnu!<CR>", { desc = "Toggle relative line number" }
 keymap.set("n", "vv", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set("n", "<leader>q", ":q<CR>", { desc = "Exit vim" })
 keymap.set("n", "<leader>E", "<cmd>Explore<CR>", { desc = "Open Explore" })
+keymap.set("n", ";tm", ":Mason<CR>", { desc = "Mason" })
+keymap.set("n", ";tl", ":Lazy<CR>", { desc = "Lazy" })
 
 -- text movement
 keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
@@ -18,18 +20,21 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+-- Split window and Select/Switch window
+keymap.set("n", "sh", ":split<CR><C-w>w", { noremap = false, desc = "Split window" })
+keymap.set("n", "sv", ":vsplit<CR><C-w>w", { noremap = false, desc = "Vsplit window" })
+keymap.set("n", "sr", ":SmartResizeMode<CR>", { noremap = false, desc = "Smart resize window" })
 
+-- Buf and Tab
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
-
--- Buf and Tab
+--- buf
 keymap.set("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true, desc = "Next Buffer" })
 keymap.set("n", "<S-Tab>", ":bprevious<CR>", { noremap = true, silent = true, desc = "Previous Buffer" })
 keymap.set("n", ";q", ":bw<CR>", { noremap = true, silent = true })
-
 keymap.set("n", "<Leader>b.", ":bnext<CR>", { noremap = true, silent = true, desc = "Next Buffer" })
 keymap.set("n", "<Leader>b,", ":bprevious<CR>", { noremap = true, silent = true, desc = "Previous Buffer" })
 keymap.set("n", "<Leader>bq", function()
@@ -43,17 +48,3 @@ keymap.set("n", "<Leader>bq", function()
 		vim.cmd(":bw")
 	end
 end, { desc = "Close Buffer" })
-
-keymap.set("n", ";tm", ":Mason<CR>", { desc = "Mason" })
-keymap.set("n", ";tl", ":Lazy<CR>", { desc = "Lazy" })
-
-local is_fold_column_show = false
-keymap.set("n", ";tf", function()
-	if is_fold_column_show then
-		vim.cmd(":set foldcolumn=0")
-	else
-		vim.cmd(":set foldcolumn=1")
-	end
-
-	is_fold_column_show = not is_fold_column_show
-end, { desc = "toggle fold column" })
