87f7a157fe4020fda4a87cb8cd6f233f4b23ac10
PinpongTp <pinpong.tp@gmail.com>
2024-04-06 12:24:14 +0700

    feat: add formatter

--- nvim/lua/pin/plugins/lsp/mason.lua
+++ nvim/lua/pin/plugins/lsp/mason.lua
@@ -1,24 +1,33 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = { "williamboman/mason-lspconfig.nvim" },
-  config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local servers = { "tsserver", "lua_ls", "gopls", "vuels", "html", "astro", "cssls", "tailwindcss", "emmet_ls" }
+	"williamboman/mason.nvim",
+	dependencies = { "williamboman/mason-lspconfig.nvim", "WhoIsSethDaniel/mason-tool-installer.nvim" },
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
+		local servers = { "tsserver", "lua_ls", "gopls", "vuels", "html", "astro", "cssls", "tailwindcss", "emmet_ls" }
 
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      ensure_installed = servers,
-    })
+		mason_lspconfig.setup({
+			ensure_installed = servers,
+		})
 
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"stylua",
+				"isort",
+				"black",
+			},
+		})
+	end,
 }
