53ef87be7b42846f0d5e4d8b01ca35733ad685fc
Pinpong Tongpat <pinpong.tp@gmail.com>
2024-05-22 01:11:44 +0700

    feat: fix not use local plugin

--- nvim/lua/pin/plugins/colorscheme.lua
+++ nvim/lua/pin/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
 	{
 		--"PinpongTp/comic",
-		--dir = "~/Developer/theme/vim/comic",
-		dir = "~/Developer/my/comic",
+		dir = "~/Developer/theme/vim/comic",
+		--dir = "~/Developer/my/comic",
 		priority = 1000,
 		config = function()
 			vim.cmd.colorscheme("comic")
