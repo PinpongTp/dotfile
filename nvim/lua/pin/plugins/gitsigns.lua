deee5142660da5b8cde2c2e13569d21e114a04f9
PinpongTp <pinpong.tp@gmail.com>
2026-02-16 11:08:09 +0700

    feat: change keymap config to lazyvim format

--- nvim/lua/pin/plugins/gitsigns.lua
+++ nvim/lua/pin/plugins/gitsigns.lua
@@ -1,39 +1,111 @@
 return {
 	"lewis6991/gitsigns.nvim",
-	config = function()
-		local gitsigns = require("gitsigns")
-
-		gitsigns.setup({
-			trouble = true,
-		})
-
-		local keymap = vim.keymap
-		keymap.set("n", ";ti", ":IBLToggle<CR>", { desc = "Toggle indent" })
-
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		trouble = true,
+	},
+	keys = {
 		-- Navigation
-		keymap.set("n", "]c", "&diff ? ']c' : ':Gitsigns next_hunk<CR>'", { expr = true })
-		keymap.set("n", "[c", "&diff ? '[c' : ':Gitsigns prev_hunk<CR>'", { expr = true })
-
+		{
+			"]c",
+			function()
+				if vim.wo.diff then
+					vim.cmd.normal({ "]c", bang = true })
+				else
+					require("gitsigns").next_hunk()
+				end
+			end,
+			desc = "Next Hunk",
+		},
+		{
+			"[c",
+			function()
+				if vim.wo.diff then
+					vim.cmd.normal({ "[c", bang = true })
+				else
+					require("gitsigns").prev_hunk()
+				end
+			end,
+			desc = "Prev Hunk",
+		},
 		-- Actions
-		keymap.set("n", ";gs", ":Gitsigns stage_hunk<CR>", { desc = "Stage hunk" })
-
-		keymap.set("v", ";gs", ":Gitsigns stage_hunk<CR>", { desc = "Stage hunk" })
-		keymap.set("n", ";gr", ":Gitsigns reset_hunk<CR>", { desc = "Reset hunk" })
-		keymap.set("v", ";gr", ":Gitsigns reset_hunk<CR>", { desc = "Reset hunk" })
-		keymap.set("n", ";gS", ":Gitsigns stage_buffer<CR>", { desc = "Stage Buffer" })
-		keymap.set("n", ";gu", ":Gitsigns undo_stage_hunk<CR>", { desc = "Undo stage hunk" })
-		keymap.set("n", ";gR", ":Gitsigns reset_buffer<CR>", { desc = "Reset Buffer" })
-		keymap.set("n", ";gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview hunk" })
-		keymap.set("n", ";gb", ":Gitsigns blame_line<CR>", { desc = "Blame line" })
-		keymap.set("n", "<C-g>", ":Gitsigns blame_line<CR>", { desc = "Blame line" })
-		keymap.set("n", ";gB", ':lua require"gitsigns".blame_line{full=true}<CR>', { desc = "Blame line full" })
-		keymap.set("n", ";gd", ":Gitsigns diffthis<CR>", { desc = "Diff this" })
-		keymap.set("n", ";gD", ':lua require"gitsigns".diffthis("~")<CR>', { desc = "Diff this full" })
-		keymap.set("n", ";tb", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle blame" })
-		keymap.set("n", ";td", ":Gitsigns toggle_deleted<CR>", { desc = "Toggle deleted" })
-
+		{
+			";gs",
+			":Gitsigns stage_hunk<CR>",
+			desc = "Stage Hunk",
+			mode = { "n", "v" },
+		},
+		{
+			";gr",
+			":Gitsigns reset_hunk<CR>",
+			desc = "Reset Hunk",
+			mode = { "n", "v" },
+		},
+		{
+			";gS",
+			":Gitsigns stage_buffer<CR>",
+			desc = "Stage Buffer",
+		},
+		{
+			";gu",
+			":Gitsigns undo_stage_hunk<CR>",
+			desc = "Undo Stage Hunk",
+		},
+		{
+			";gR",
+			":Gitsigns reset_buffer<CR>",
+			desc = "Reset Buffer",
+		},
+		{
+			";gp",
+			":Gitsigns preview_hunk<CR>",
+			desc = "Preview Hunk",
+		},
+		{
+			";gb",
+			":Gitsigns blame_line<CR>",
+			desc = "Blame Line",
+		},
+		{
+			"<C-g>",
+			":Gitsigns blame_line<CR>",
+			desc = "Blame Line",
+		},
+		{
+			";gB",
+			function()
+				require("gitsigns").blame_line({ full = true })
+			end,
+			desc = "Blame Line (Full)",
+		},
+		{
+			";gd",
+			":Gitsigns diffthis<CR>",
+			desc = "Diff This",
+		},
+		{
+			";gD",
+			function()
+				require("gitsigns").diffthis("~")
+			end,
+			desc = "Diff This (Full)",
+		},
+		{
+			";tb",
+			":Gitsigns toggle_current_line_blame<CR>",
+			desc = "Toggle Blame",
+		},
+		{
+			";td",
+			":Gitsigns toggle_deleted<CR>",
+			desc = "Toggle Deleted",
+		},
 		-- Text object
-		keymap.set("o", "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select hunk" })
-		keymap.set("x", "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select hunk" })
-	end,
+		{
+			"ih",
+			":<C-U>Gitsigns select_hunk<CR>",
+			desc = "Select Hunk",
+			mode = { "o", "x" },
+		},
+	},
 }
