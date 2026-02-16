7be21458ff9200d35312f5d6079fcb3fdc865a2b
PinpongTp <pinpong.tp@gmail.com>
2026-02-16 11:08:34 +0700

    fest: format code

--- nvim/lua/pin/plugins/toggleterm.lua
+++ nvim/lua/pin/plugins/toggleterm.lua
@@ -1,30 +1,36 @@
 return {
 	"akinsho/toggleterm.nvim",
-  config = function()
-    tg = require("toggleterm")
+	config = function()
+		tg = require("toggleterm")
 
-    tg.setup({
-      open_mapping = [[<c-x>]],
-      shade_terminals = true,
-      close_on_exit = true,
-      direction = "float",
-      float_opts = {
-        border = "curved",
-        whiblend = 0,
-        highlights = { border = "Normal", backgroud = "Normal" },
-      },
-    })
+		tg.setup({
+			open_mapping = [[<c-x>]],
+			shade_terminals = true,
+			close_on_exit = true,
+			direction = "float",
+			float_opts = {
+				border = "curved",
+				whiblend = 0,
+				highlights = { border = "Normal", backgroud = "Normal" },
+			},
+		})
 
-    local Terminal = require("toggleterm.terminal").Terminal
-    local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({
+			cmd = "lazygit",
+			hidden = true,
+			env = {
+				NVIM = vim.v.servername, -- สำคัญ: ทำให้ lazygit ยิงกลับเข้า nvim instance เดิมได้
+			},
+		})
 
-    function _lazygit_toggle()
-      lazygit:toggle()
-    end
+		function _lazygit_toggle()
+			lazygit:toggle()
+		end
 
-    local keymap = vim.keymap
+		local keymap = vim.keymap
 
-    keymap.set("n", ";tg", "<Cmd>lua _lazygit_toggle()<CR>", { desc = "Lazygit" })
-    keymap.set("n", "<c-z>", ":ToggleTerm direction=horizontal<CR>", { desc = "Zoom" })
-  end
+		keymap.set("n", ";tg", "<Cmd>lua _lazygit_toggle()<CR>", { desc = "Lazygit" })
+		keymap.set("n", "<c-z>", ":ToggleTerm direction=horizontal<CR>", { desc = "Zoom" })
+	end,
 }
