5f4172e7059691e478285d889a137ba0b3057da2
Pinpong Tongpat <pinpong.tp@gmail.com>
2026-02-28 15:42:01 +0700

    feat: remove old vim config

--- _nvim/lua/plugin_configs/lsp/init.lua
+++
@@ -1,7 +0,0 @@
-require("plugin_configs.lsp.lspkind")
-require("plugin_configs.lsp.cmp")
-require("plugin_configs.lsp.config")
-require("plugin_configs.lsp.null-ls")
-require("plugin_configs.lsp.saga")
-require("plugin_configs.lsp.toggle-lsp-diagnostics")
-require("plugin_configs.lsp.fidget")
