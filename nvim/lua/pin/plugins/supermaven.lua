3298b7452b775c7818504feb06be961ac6739d80
Pinpong Tongpat <pinpong.tp@gmail.com>
2026-06-12 16:08:57 +0700

    feat(nvim): replace copilot with supermaven for completion

---
+++ nvim/lua/pin/plugins/supermaven.lua
@@ -0,0 +1,32 @@
+return {
+	{
+		"supermaven-inc/supermaven-nvim",
+		event = "InsertEnter",
+		config = function()
+			require("supermaven-nvim").setup({
+				keymaps = {
+					accept_suggestion = "<Right>",
+					clear_suggestion = "<C-l>",
+					accept_word = "<C-j>",
+				},
+				ignore_filetypes = {
+					yaml = true,
+					markdown = true,
+					help = true,
+					gitcommit = true,
+					gitrebase = true,
+					hgcommit = true,
+					svn = true,
+					cvs = true,
+				},
+				color = {
+					suggestion_color = "#808080",
+					cterm = 244,
+				},
+				log_level = "off",
+				disable_inline_completion = false,
+				disable_keymaps = false,
+			})
+		end,
+	},
+}
